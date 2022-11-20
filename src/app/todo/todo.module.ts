de5a324a40b751bd990776c031cb931966071459
tane-git <tane.alexander.wilson@gmail.com>
2022-11-20 15:36:03 +1300

    '$ ng generate module todo'

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, //
+    MatSlideToggleModule,
+  ],
+})
+export class TodoModule {}
